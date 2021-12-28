add7fd6a8c70f2b9e1ee3f6a8ea9764b01c7f138
James Benner <james.benner@gmail.com>
2021-12-28 14:21:40 -0500

    Update VersionProperty reference comment

--- lib/properties/VersionProperty.ts
+++ lib/properties/VersionProperty.ts
@@ -6,6 +6,24 @@ export type VersionPropertyLike = VersionProperty | number;
 const VALUE: unique symbol = Symbol.for('value');
 
 /**
+ * > Purpose:  To specify the version of the vCard specification used to
+ * >   format this vCard.
+ * >
+ * > Value type:  A single text value.
+ * >
+ * > Special notes:  This property MUST be present in the vCard object,
+ * >   and it must appear immediately after BEGIN:VCARD. The value MUST
+ * >   be "4.0" if the vCard corresponds to this specification. Note
+ * >   that earlier versions of vCard allowed this property to be placed
+ * >   anywhere in the vCard object, or even to be absent.
+ * >
+ * > ABNF:
+ * >   VERSION-param = "VALUE=text" / any-param
+ * >   VERSION-value = "4.0"
+ * >
+ * > Example:
+ * >   VERSION:4.0
+ *
  * @see https://datatracker.ietf.org/doc/html/rfc6350#section-6.7.9
  */
 export default class VersionProperty extends Property {
