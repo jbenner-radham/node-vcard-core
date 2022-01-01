9d60ea144332d3f5a385e5fccf29967009721112
James Benner <james.benner@gmail.com>
2022-01-01 16:05:50 -0500

    Update NProperty reference comment

--- lib/properties/NProperty.ts
+++ lib/properties/NProperty.ts
@@ -13,6 +13,35 @@ export type NPropertyLike = NProperty | string;
 const VALUE: unique symbol = Symbol.for('value');
 
 /**
+ * > Purpose:  To specify the components of the name of the object the
+ * >   vCard represents.
+ * >
+ * > Value type:  A single structured text value. Each component can have
+ * >   multiple values.
+ * >
+ * > Special note:  The structured property value corresponds, in
+ * >   sequence, to the Family Names (also known as surnames), Given
+ * >   Names, Additional Names, Honorific Prefixes, and Honorific
+ * >   Suffixes. The text components are separated by the SEMICOLON
+ * >   character (U+003B). Individual text components can include
+ * >   multiple text values separated by the COMMA character (U+002C).
+ * >   This property is based on the semantics of the X.520 individual
+ * >   name attributes [CCITT.X520.1988]. The property SHOULD be present
+ * >   in the vCard object when the name of the object the vCard
+ * >   represents follows the X.520 model.
+ * >
+ * >   The SORT-AS parameter MAY be applied to this property.
+ * >
+ * > ABNF:
+ * >   N-param = "VALUE=text" / sort-as-param / language-param
+ * >           / altid-param / any-param
+ * >   N-value = list-component 4(";" list-component)
+ * >
+ * > Examples:
+ * >   N:Public;John;Quinlan;Mr.;Esq.
+ * >
+ * >   N:Stevenson;John;Philip,Paul;Dr.;Jr.,M.D.,A.C.P.
+ *
  * @see https://datatracker.ietf.org/doc/html/rfc6350#section-6.2.2
  */
 export default class NProperty extends Property {
