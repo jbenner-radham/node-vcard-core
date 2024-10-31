5376541ec607f6a2b97b2c1978f4d2159ea513fe
James Benner <james.benner@gmail.com>
2024-10-31 01:05:17 -0400

    Add getInvalidPidParameterMessage function

--- packages/vcard4-generator/lib/util/error-messages.ts
+++ packages/vcard4-generator/lib/util/error-messages.ts
@@ -15,6 +15,12 @@ export function getInvalidMediatypeValueParameterMessage({ value }: { value: any
     return `The MEDIATYPE parameter is only valid for "uri" value types. The value type of "${value}" was provided`;
 }
 
+export function getInvalidPidParameterMessage({ pid }: { pid: unknown }): string {
+    return 'The PID parameter must be an integer consisting of one digit, a float consisting of one digit on both ' +
+        `sides of the decimal point, or an array containing the aforementioned number formats. The value "${pid}" ` +
+        'was provided';
+}
+
 export function getInvalidPrefParameterMessage({ pref }: { pref: any }): string {
     return `The PREF parameter must be an integer between 1 and 100. The value ${pref} was provided`;
 }
