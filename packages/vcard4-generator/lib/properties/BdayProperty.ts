0cf0b8a938bd3e144bdaaff71e7e59e3567f1a15
James Benner <james.benner@gmail.com>
2024-11-05 13:52:13 -0500

    Add new BDAY parameters

--- packages/vcard4-generator/lib/properties/BdayProperty.ts
+++ packages/vcard4-generator/lib/properties/BdayProperty.ts
@@ -1,4 +1,4 @@
-import type { Altid, Calscale, Cardinality, Group, Options, Value } from '../types.js';
+import type { Altid, Calscale, Cardinality, CommonParameters, Group, Options, Value } from '../types.js';
 import {
     getInvalidCalscaleValueParameterMessage,
     getInvalidLanguageValueParameterMessage
@@ -9,7 +9,7 @@ import Property from './Property.js';
 
 type BdayCommonParameters = {
     altid?: Altid;
-};
+} & CommonParameters;
 
 type BdayDateAndOrTimeOrUndefinedValueParameters = {
     value?: Extract<Value, 'date-and-or-time'>;
