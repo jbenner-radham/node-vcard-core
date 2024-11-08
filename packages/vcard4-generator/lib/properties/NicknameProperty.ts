baefe29cec2ffe553052f2b04b1162ca09945b84
James Benner <james.benner@gmail.com>
2024-11-08 12:23:18 -0500

    Add new NICKNAME parameters

--- packages/vcard4-generator/lib/properties/NicknameProperty.ts
+++ packages/vcard4-generator/lib/properties/NicknameProperty.ts
@@ -1,4 +1,4 @@
-import type { Altid, Cardinality, Group, Pid, Pref, Options, Type, Value } from '../types.js';
+import type { Altid, Cardinality, CommonParameters, Group, Pid, Pref, Options, Type, Value } from '../types.js';
 import { getInvalidPidParameterMessage, getInvalidPrefParameterMessage } from '../util/error-messages.js';
 import isString from '../util/is-string.js';
 import isValidGroup from '../util/is-valid-group.js';
@@ -6,14 +6,14 @@ import isValidPidParameter from '../util/is-valid-pid-parameter.js';
 import isValidPrefParameter from '../util/is-valid-pref-parameter.js';
 import Property from './Property.js';
 
-export interface NicknameParameters {
+export type NicknameParameters = {
     value?: Extract<Value, 'text'>;
     type?: Type;
     language?: string;
     altid?: Altid;
     pid?: Pid;
     pref?: Pref;
-}
+} & CommonParameters;
 
 export type NicknameRestConfig = [value: string, parameters?: NicknameParameters, options?: Options];
 
