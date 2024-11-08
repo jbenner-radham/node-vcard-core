86c6d19114c76c5c38786b3c81c166e37fba7454
James Benner <james.benner@gmail.com>
2024-11-08 12:16:11 -0500

    Add new LOGO parameters

--- packages/vcard4-generator/lib/properties/LogoProperty.ts
+++ packages/vcard4-generator/lib/properties/LogoProperty.ts
@@ -1,4 +1,4 @@
-import type { Altid, Cardinality, Group, Pid, Pref, Options, Type, Value } from '../types.js';
+import type { Altid, Cardinality, CommonParameters, Group, Pid, Pref, Options, Type, Value } from '../types.js';
 import { getInvalidPidParameterMessage, getInvalidPrefParameterMessage } from '../util/error-messages.js';
 import isString from '../util/is-string.js';
 import isValidGroup from '../util/is-valid-group.js';
@@ -6,7 +6,7 @@ import isValidPidParameter from '../util/is-valid-pid-parameter.js';
 import isValidPrefParameter from '../util/is-valid-pref-parameter.js';
 import Property from './Property.js';
 
-export interface LogoParameters {
+export type LogoParameters = {
     value?: Extract<Value, 'uri'>;
     language?: string;
     pid?: Pid;
@@ -14,7 +14,7 @@ export interface LogoParameters {
     type?: Type;
     mediatype?: string;
     altid?: Altid;
-}
+} & CommonParameters;
 
 export type LogoRestConfig = [value: string, parameters?: LogoParameters, options?: Options];
 
